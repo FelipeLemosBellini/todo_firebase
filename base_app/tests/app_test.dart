08452b21809cf67ab466ce33625e8f52276ded25
felipe bellini <felipelemosbellini@gmail.com>
2022-08-01 09:18:25 -0300

    add test modular

--- base_app/tests/app_test.dart
+++ base_app/tests/app_test.dart
@@ -6,15 +6,21 @@ import 'package:module_auth/module/auth_module.dart';
 import 'package:module_commons/helpers/whatsapp_meetup_routes.dart';
 import 'package:module_external_dependencies/modular_test.dart';
 import 'package:module_todo/module/module_chat.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+
+// import 'package:flutter_modular/flutter_modular.dart';
+import 'package:base_app/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
 
   setUp(() async {
-    Modular.init(AuthModule());
-    Modular.setInitialRoute(WhatsAppMeetupRoutes.loginPage);
-    await
+    // app.main();
+    // Modular.init(AuthModule());
+    // Modular.setInitialRoute(WhatsAppMeetupRoutes.loginPage);
+  });
+  testWidgets("buttonLogin", (WidgetTester test) async {
+    app.main();
+    await test.tap(find.byKey(const Key("buttonLogin")));
   });
 }
